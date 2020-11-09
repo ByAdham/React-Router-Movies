ced9a8b7eee5a4a0faaca935529538263a40261d
Adham AbdelFattah <adham.adel@gmail.com>
2020-11-09 16:18:57 -0600

    just started

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,5 @@ import App from './App';
 import { Router } from 'express';
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(
-    <Router>   <App /> 
-    </Router>, document.getElementById('root')
-    
+ReactDOM.render(<Router> <App /> </Router>, document.getElementById('root')    
 );
